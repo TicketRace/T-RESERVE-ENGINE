70dd2f4549fd7688a28851e47b4dbb44b7e40877
K0taka <144228630+K0taka@users.noreply.github.com>
2026-05-14 18:22:34 +0500

    feat: исключения согласно спецификации

---
+++ src/main/java/com/treserve/common/exception/ForbiddenOperationException.java
@@ -0,0 +1,8 @@
+package com.treserve.common.exception;
+
+public class ForbiddenOperationException extends RuntimeException {
+
+    public ForbiddenOperationException(String message) {
+        super(message);
+    }
+}
